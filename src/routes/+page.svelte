d4e0456433825eb3a053328c8ee7c7b6f469f78e
DAI-KON <d2a7d2al7ps@gmail.com>
2025-09-05 18:56:32 +0900

    design1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
 <script>
+
 </script>
+
+<main>
+    <div class="flex justify-center-safe p-20 gap-[100px]">
+
+        <div class="rounded-4xl h-100 w-100 border-3 border-gray-300 p-5">
+
+            <h1 class="p-2 grid grid-flow-col justify-items-center font-bold text-4xl">総合ランキング</h1>
+
+            <div class="bg-gray-200 rounded-4xl h-[300px] w-full">
+                
+            </div>
+            
+        </div>
+
+        <div class="rounded-4xl h-100 w-100 border-3 border-gray-300 p-10">
+
+            
+
+        </div>
+
+    </div>
+</main>
